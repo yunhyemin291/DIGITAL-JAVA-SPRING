66e3d900689e3f8606d865568994b43fbd9ab023
yunhyemin <yunhyemin15@gmail.com>
2020-07-28 10:06:48 +0900

    spring3 타일즈설정

--- springtest2/src/main/java/kr/green/springtest/HomeController.java
+++ springtest2/src/main/java/kr/green/springtest/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Handles requests for the application home page.
@@ -35,5 +36,11 @@ public class HomeController {
 		
 		return "home";
 	}
-	
+	@RequestMapping(value= {"/main/home","/home.do"})
+	public ModelAndView openTilesView(ModelAndView mv) throws Exception{
+	    mv.setViewName("/main/home");
+	    mv.addObject("setHeader", "타일즈");
+	    return mv;
+	}
+
 }
