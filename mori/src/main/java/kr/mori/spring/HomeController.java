9b5e8626bc17e9f33ecb4fd05a4cd6c7c164e4fa
yunhyemin <yunhyemin15@gmail.com>
2020-08-24 17:37:14 +0900

    mori-타일즈설정

--- mori/src/main/java/kr/mori/spring/HomeController.java
+++ mori/src/main/java/kr/mori/spring/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Handles requests for the application home page.
@@ -19,21 +20,10 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+	@RequestMapping(value= {"/main/home","/home.do"})
+	public ModelAndView openTilesView(ModelAndView mv) throws Exception{
+	    mv.setViewName("/main/home");
+	    mv.addObject("setHeader", "타일즈");
+	    return mv;
 	}
-	
 }
